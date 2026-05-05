5eb6e3392c71a5996673f2d8b3e9b7c25c2842be
Wilfried Musanzi <musanziw.m@gmail.com>
2026-05-05 13:29:55 +0200

    refactor: add return types to reservation controller methods for better type safety

--- apps/reservations/src/reservations.controller.ts
+++ apps/reservations/src/reservations.controller.ts
@@ -1,42 +1,35 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { Reservation } from './entities/reservation.entity';
 
 @Controller('reservations')
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
 
   @Post()
-  create(@Body() dto: CreateReservationDto) {
+  create(@Body() dto: CreateReservationDto): Promise<Reservation> {
     return this.reservationsService.create(dto);
   }
 
   @Get()
-  findAll() {
+  findAll(): Promise<Reservation[]> {
     return this.reservationsService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: string): Promise<Reservation> {
     return this.reservationsService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() dto: UpdateReservationDto) {
+  update(@Param('id') id: string, @Body() dto: UpdateReservationDto): Promise<Reservation> {
     return this.reservationsService.update(id, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: string): Promise<void> {
     return this.reservationsService.remove(id);
   }
 }
