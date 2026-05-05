ce0c623432686a91a785b7ef477c67962226a755
Wilfried Musanzi <musanziw.m@gmail.com>
2026-05-05 13:31:27 +0200

    chore: add validation decorators for startDate and endDate in CreateReservationDto

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,4 +1,12 @@
+import { IsDate } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @IsDate()
+  @Type(() => Date)
   startDate!: Date;
+
+  @IsDate()
+  @Type(() => Date)
   endDate!: Date;
 }
