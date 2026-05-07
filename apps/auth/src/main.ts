676b9bf88c2c8611efa0e4bf2b8a892e6bd5f64f
Wilfried Musanzi <musanziw.m@gmail.com>
2026-05-07 12:29:05 +0200

    fix(auth): correct import path and enhance ValidationPipe configuration

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,12 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { Logger } from 'nestjs-pino';
-import { AuthModule } from './auth.module';
+import { AuthModule } from './auth/auth.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useLogger(app.get(Logger));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.AUTH_PORT ?? 3000);
 }
 bootstrap().catch(() => {});
