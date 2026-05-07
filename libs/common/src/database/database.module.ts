336ebdbc3d6a95463dc0841aec52d4d1932bb623
Wilfried Musanzi <musanziw.m@gmail.com>
2026-05-07 15:10:25 +0200

    fix(database): improve database connection configuration for Docker support

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -9,15 +9,22 @@ import { EntityClassOrSchema } from '@nestjs/typeorm/dist/interfaces/entity-clas
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get('DB_HOST'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        username: configService.get('DB_USERNAME'),
-        autoLoadEntities: true,
-        synchronize: true
-      })
+      useFactory: (configService: ConfigService) => {
+        const isDocker = configService.get<string>('DOCKERIZED') === 'true';
+        const host = configService.get<string>('DB_HOST') || (isDocker ? 'sleepr-db' : '127.0.0.1');
+        const port = Number(configService.get<string>('DB_PORT') || 3306);
+
+        return {
+          type: 'mysql',
+          host,
+          port,
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          username: configService.get('DB_USERNAME'),
+          autoLoadEntities: true,
+          synchronize: true
+        };
+      }
     })
   ]
 })
